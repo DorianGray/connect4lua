8fc124494d8c570c1ac38073e96ef09b077387aa
Drew Ditthardt <drew.ditthardt@gmail.com>
2012-11-27 12:25:39 -0800

    little more cleanup

--- libconnect4.lua
+++ libconnect4.lua
@@ -13,7 +13,6 @@ local function setPlayerAt(coords, player)
   lib.grid[coords.x][coords.y] = player
 end
 
---initialize grid to 0
 lib.init = function(sizeX, sizeY)
   sizeX = sizeX or lib.sizeX
   sizeY = sizeY or lib.sizeY
@@ -25,7 +24,6 @@ lib.init = function(sizeX, sizeY)
   end
 end
 
---adds a piece to the grid, stacks on top
 lib.addPiece = function(x, player)
   if x > 0 and x <= lib.sizeX then
     local currCol = lib.grid[x]
@@ -36,20 +34,15 @@ lib.addPiece = function(x, player)
       end
     end
   end
-  return nil
 end
 
---checks to see if a player has a piece at coordinates
 local function checkPlayerAt(coords, player)
-  if coords.x > 0 and coords.y > 0 and
-    coords.x <= lib.sizeX and coords.y <= lib.sizeY and
-    getPlayerAt(coords) == player then
-    return true
-  end
-  return false
+  return
+  coords.x > 0 and coords.y > 0 and
+  coords.x <= lib.sizeX and coords.y <= lib.sizeY and
+  getPlayerAt(coords) == player
 end
 
---checks a particular vector for a win
 local function checkVector(coords, mod)
   local found, i, pass1, pass2 = 1, 0, true, true
   local player = getPlayerAt(coords)
@@ -65,18 +58,11 @@ local function checkVector(coords, mod)
     -- no pass : end
     found = found + ((pass1 and pass2) and 2 or (pass1 or pass2) and 1 or 0)
   end
-
-  if found >= lib.sizeWin then
-    return true
-  else
-    return false
-  end
+  return found >= lib.sizeWin
 end
 
---checks for a win at coordinates
 lib.checkWin = function(coords)
-  --check all 4 win vectors
-  return
+  return --check all 4 win vectors
   checkVector(coords, {x=0, y=1}) or --vertical
   checkVector(coords, {x=1, y=0}) or --horizontal
   checkVector(coords, {x=1, y=1}) or --diagonal slope: 1
